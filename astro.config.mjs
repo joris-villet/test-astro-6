97473b490aa44a62bf3488dac825a155c4ddcfeb
joris villet <julek.krakow@proton.me>
2026-01-30 14:06:26 +0100

    basic project

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,15 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import tailwindcss from "@tailwindcss/vite";
+import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [],
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
+  output: "server",
+  adapter: cloudflare(),
+});
